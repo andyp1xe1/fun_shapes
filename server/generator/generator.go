548ecf8d042e3b70947666bf14025928bb5e3d4a
Andre Chicu <andriesh.chicu@gmail.com>
2024-04-16 19:09:16 +0300

    simplified rotation

--- server/generator/generator.go
+++ server/generator/generator.go
@@ -13,9 +13,9 @@ type Options struct {
 
 var Opts = Options{
 	//NumColors:       256,
-	NumSolidShapes:  200,
-	NumOpaqueShapes: 30,
-	NumMonteShapes:  80,
+	NumSolidShapes:  550,
+	NumOpaqueShapes: 50,
+	NumMonteShapes:  25,
 	MonteDensity:    0.4,
 	PopulationSize:  150,
 	InPath:          "./img_test/in.png",
